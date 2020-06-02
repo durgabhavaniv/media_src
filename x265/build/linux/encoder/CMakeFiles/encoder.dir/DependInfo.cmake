5f4bca82c64b7b174edef718b9c4e2539d892602
wms <sas@172.20.40.123>
2020-06-02 22:18:22 +0530

    new2

---
+++ x265/build/linux/encoder/CMakeFiles/encoder.dir/DependInfo.cmake
@@ -0,0 +1,53 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/sas/media_src/x265/source/encoder/analysis.cpp" "/home/sas/media_src/x265/build/linux/encoder/CMakeFiles/encoder.dir/analysis.cpp.o"
+  "/home/sas/media_src/x265/source/encoder/api.cpp" "/home/sas/media_src/x265/build/linux/encoder/CMakeFiles/encoder.dir/api.cpp.o"
+  "/home/sas/media_src/x265/source/encoder/bitcost.cpp" "/home/sas/media_src/x265/build/linux/encoder/CMakeFiles/encoder.dir/bitcost.cpp.o"
+  "/home/sas/media_src/x265/source/encoder/dpb.cpp" "/home/sas/media_src/x265/build/linux/encoder/CMakeFiles/encoder.dir/dpb.cpp.o"
+  "/home/sas/media_src/x265/source/encoder/encoder.cpp" "/home/sas/media_src/x265/build/linux/encoder/CMakeFiles/encoder.dir/encoder.cpp.o"
+  "/home/sas/media_src/x265/source/encoder/entropy.cpp" "/home/sas/media_src/x265/build/linux/encoder/CMakeFiles/encoder.dir/entropy.cpp.o"
+  "/home/sas/media_src/x265/source/encoder/frameencoder.cpp" "/home/sas/media_src/x265/build/linux/encoder/CMakeFiles/encoder.dir/frameencoder.cpp.o"
+  "/home/sas/media_src/x265/source/encoder/framefilter.cpp" "/home/sas/media_src/x265/build/linux/encoder/CMakeFiles/encoder.dir/framefilter.cpp.o"
+  "/home/sas/media_src/x265/source/encoder/level.cpp" "/home/sas/media_src/x265/build/linux/encoder/CMakeFiles/encoder.dir/level.cpp.o"
+  "/home/sas/media_src/x265/source/encoder/motion.cpp" "/home/sas/media_src/x265/build/linux/encoder/CMakeFiles/encoder.dir/motion.cpp.o"
+  "/home/sas/media_src/x265/source/encoder/nal.cpp" "/home/sas/media_src/x265/build/linux/encoder/CMakeFiles/encoder.dir/nal.cpp.o"
+  "/home/sas/media_src/x265/source/encoder/ratecontrol.cpp" "/home/sas/media_src/x265/build/linux/encoder/CMakeFiles/encoder.dir/ratecontrol.cpp.o"
+  "/home/sas/media_src/x265/source/encoder/reference.cpp" "/home/sas/media_src/x265/build/linux/encoder/CMakeFiles/encoder.dir/reference.cpp.o"
+  "/home/sas/media_src/x265/source/encoder/sao.cpp" "/home/sas/media_src/x265/build/linux/encoder/CMakeFiles/encoder.dir/sao.cpp.o"
+  "/home/sas/media_src/x265/source/encoder/search.cpp" "/home/sas/media_src/x265/build/linux/encoder/CMakeFiles/encoder.dir/search.cpp.o"
+  "/home/sas/media_src/x265/source/encoder/sei.cpp" "/home/sas/media_src/x265/build/linux/encoder/CMakeFiles/encoder.dir/sei.cpp.o"
+  "/home/sas/media_src/x265/source/encoder/slicetype.cpp" "/home/sas/media_src/x265/build/linux/encoder/CMakeFiles/encoder.dir/slicetype.cpp.o"
+  "/home/sas/media_src/x265/source/encoder/weightPrediction.cpp" "/home/sas/media_src/x265/build/linux/encoder/CMakeFiles/encoder.dir/weightPrediction.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "EXPORT_C_API=1"
+  "HAVE_INT_TYPES_H=1"
+  "HIGH_BIT_DEPTH=0"
+  "X265_ARCH_X86=1"
+  "X265_DEPTH=8"
+  "X265_NS=x265"
+  "X86_64=1"
+  "__STDC_LIMIT_MACROS=1"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/home/sas/media_src/x265/source/."
+  "/home/sas/media_src/x265/source/common"
+  "/home/sas/media_src/x265/source/encoder"
+  "."
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ x265/build/linux/make-Makefiles.bash
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Run this from within a bash shell
+/usr/bin/cmake -G "Unix Makefiles" ../../source && ccmake ../../source
